d88f16e0275f0eb4b4b0809609eec1c34c3059bf
Artem Islamov <artem.islamov@eigenmethod.com>
2023-02-09 22:01:43 +0000

    update  RentCar and RentCarInfo components

--- src/components/RentCar.js
+++ src/components/RentCar.js
@@ -1,42 +1,20 @@
 import Navbar from "./Navbar";
-import NFTTile from "./NFTTile";
-import MarketplaceJSON from "../Marketplace.json";
+import RentedCarTile from "./RentedCarTile";
+import RentCarJSON from "../RentCar.json";
 import axios from "axios";
 import { useState } from "react";
 
 export default function RentCar() {
-const sampleData = [
-    {
-        "name": "NFT#1",
-        "description": "Alchemy's First NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
-    },
-    {
-        "name": "NFT#2",
-        "description": "Alchemy's Second NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmdhoL9K8my2vi3fej97foiqGmJ389SMs55oC5EdkrxF2M",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-    }
-];
-const [data, updateData] = useState(sampleData);
+const [data, updateData] = useState([]);
 const [dataFetched, updateFetched] = useState(false);
 
-async function getAllNFTs() {
+async function getAllAvailableCars() {
     const ethers = require("ethers");
-    //After adding your Hardhat network to your metamask, this code will get providers and signers
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    //Pull the deployed contract instance
-    let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer)
-    //create an NFT Token
-    let transaction = await contract.getAllNFTs()
+    let contract = new ethers.Contract(RentCarJSON.address, RentCarJSON.abi, signer)
+    
+    let transaction = await contract.getAllAvailableCars()
 
     //Fetch all the details of every NFT from the contract and display
     const items = await Promise.all(transaction.map(async i => {
@@ -44,7 +22,7 @@ async function getAllNFTs() {
         let meta = await axios.get(tokenURI);
         meta = meta.data;
 
-        let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+        let price = ethers.utils.formatUnits(i.pricePerDay.toString(), 'ether');
         let item = {
             price,
             tokenId: i.tokenId.toNumber(),
@@ -52,6 +30,7 @@ async function getAllNFTs() {
             owner: i.owner,
             image: meta.image,
             name: meta.name,
+            model: meta.model,
             description: meta.description,
         }
         return item;
@@ -62,7 +41,7 @@ async function getAllNFTs() {
 }
 
 if(!dataFetched)
-    getAllNFTs();
+    getAllAvailableCars();
 
 return (
     <div>
@@ -73,7 +52,7 @@ return (
             </div>
             <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
                 {data.map((value, index) => {
-                    return <NFTTile data={value} key={index}></NFTTile>;
+                    return <RentedCarTile data={value} key={index}></RentedCarTile>;
                 })}
             </div>
         </div>            
--- src/components/RentCarInfo.js
+++ src/components/RentCarInfo.js
@@ -1,49 +1,57 @@
 import Navbar from "./Navbar";
 import axie from "../tile.jpeg";
 import { useLocation, useParams } from 'react-router-dom';
-import MarketplaceJSON from "../Marketplace.json";
+import RentCarJSON from "../RentCar.json";
 import axios from "axios";
 import { useState } from "react";
 
 export default function RentCarInfo (props) {
 
 const [data, updateData] = useState({});
+const [days, updateDays] = useState(1);
+const [totalPrice, updateTotalPrice] = useState(0);
 const [dataFetched, updateDataFetched] = useState(false);
 const [message, updateMessage] = useState("");
 const [currAddress, updateCurrAddress] = useState("0x");
 
-async function getNFTData(tokenId) {
+function updateRentForDays(value){
+    updateDays(value);
+    updateTotalPrice(value * data.price);
+}
+
+async function getCarData(tokenId) {
     const ethers = require("ethers");
-    //After adding your Hardhat network to your metamask, this code will get providers and signers
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const addr = await signer.getAddress();
-    //Pull the deployed contract instance
-    let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer)
-    //create an NFT Token
+    let contract = new ethers.Contract(RentCarJSON.address, RentCarJSON.abi, signer)
+    
     const tokenURI = await contract.tokenURI(tokenId);
-    const listedToken = await contract.getListedTokenForId(tokenId);
+    const listedToken = await contract.getCarToRentForId(tokenId);
+    let price = ethers.utils.formatUnits(listedToken.pricePerDay.toString(), 'ether');
     let meta = await axios.get(tokenURI);
     meta = meta.data;
     console.log(listedToken);
 
     let item = {
-        price: meta.price,
+        price: price,
         tokenId: tokenId,
-        seller: listedToken.seller,
         owner: listedToken.owner,
         image: meta.image,
         name: meta.name,
+        model: meta.model,
         description: meta.description,
     }
     console.log(item);
     updateData(item);
+    updateDays(1);
+    updateTotalPrice(price);
     updateDataFetched(true);
     console.log("address", addr)
     updateCurrAddress(addr);
 }
 
-async function buyNFT(tokenId) {
+async function rentCar(tokenId) {
     try {
         const ethers = require("ethers");
         //After adding your Hardhat network to your metamask, this code will get providers and signers
@@ -51,14 +59,14 @@ async function buyNFT(tokenId) {
         const signer = provider.getSigner();
 
         //Pull the deployed contract instance
-        let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer);
-        const salePrice = ethers.utils.parseUnits(data.price, 'ether')
+        let contract = new ethers.Contract(RentCarJSON.address, RentCarJSON.abi, signer);
+        const rentPrice = ethers.utils.parseUnits(totalPrice.toString(), 'ether')
         updateMessage("Buying the NFT... Please Wait (Upto 5 mins)")
         //run the executeSale function
-        let transaction = await contract.executeSale(tokenId, {value:salePrice});
+        let transaction = await contract.rentCar(tokenId, days, {value:rentPrice});
         await transaction.wait();
 
-        alert('You successfully bought the NFT!');
+        alert('You successfully rent this car!');
         updateMessage("");
     }
     catch(e) {
@@ -69,7 +77,7 @@ async function buyNFT(tokenId) {
     const params = useParams();
     const tokenId = params.tokenId;
     if(!dataFetched)
-        getNFTData(tokenId);
+        getCarData(tokenId);
 
     return(
         <div style={{"min-height":"100vh"}}>
@@ -81,6 +89,9 @@ async function buyNFT(tokenId) {
                         Name: {data.name}
                     </div>
                     <div>
+                        Model: {data.model}
+                    </div>
+                    <div>
                         Description: {data.description}
                     </div>
                     <div>
@@ -90,13 +101,13 @@ async function buyNFT(tokenId) {
                         Owner: <span className="text-sm">{data.owner}</span>
                     </div>
                     <div>
-                        Seller: <span className="text-sm">{data.seller}</span>
+                        Rent for <input className="shadow appearance-none border rounded w-1/5 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" placeholder="Min 1 day" step="1" value={days} onChange={e => updateRentForDays(e.target.value)}></input> days
                     </div>
                     <div>
-                    { currAddress == data.owner || currAddress == data.seller ?
-                        <button className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm" onClick={() => buyNFT(tokenId)}>Buy this NFT</button>
-                        : <div className="text-emerald-700">You are the owner of this NFT</div>
-                    }
+                        Total Price: <span className="">{totalPrice + " ETH"}</span>
+                    </div>
+                    <div>
+                    <button className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm" onClick={() => rentCar(tokenId)}>Rent this car</button>
                     
                     <div className="text-green text-center mt-3">{message}</div>
                     </div>
