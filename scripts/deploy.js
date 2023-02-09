b9c6f4ada01583ebeb590e0eee0e747d91aa38a7
Artem Islamov <artem.islamov@eigenmethod.com>
2023-02-09 21:59:57 +0000

    add base test file and update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,18 +5,25 @@ const fs = require("fs");
 async function main() {
   const [deployer] = await ethers.getSigners();
   const balance = await deployer.getBalance();
+  console.log("Deployer address is:", deployer.getAddress()," with balance:", balance);
   const RentCar = await hre.ethers.getContractFactory("RentCar");
   const rentCar = await RentCar.deploy();
 
   await rentCar.deployed();
+  console.log("RentCar deployed to:", rentCar.address);
 
-  const data = {
-    address: rentCar.address,
-    abi: JSON.parse(rentCar.interface.format('json'))
+  const chainId = (await deployer.provider?.getNetwork()).chainId;
+  if (chainId !== 1337)
+  {
+    const data = {
+      address: rentCar.address,
+      abi: JSON.parse(rentCar.interface.format('json'))
+    }
+  
+    //This writes the ABI and address to the mktplace.json
+    fs.writeFileSync('./src/RentCar.json', JSON.stringify(data))
+    console.log("JSON abi saved to ./src/RentCar.json");
   }
-
-  //This writes the ABI and address to the mktplace.json
-  fs.writeFileSync('./src/RentCar.json', JSON.stringify(data))
 }
 
 main()
