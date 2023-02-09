9ea0f7258a4aaf1a2c2dcc161799bd878c0ac423
Artem Islamov <artem.islamov@eigenmethod.com>
2023-02-09 23:35:57 +0000

    add  RequestTile and show requests logic for host

--- src/components/Host.js
+++ src/components/Host.js
@@ -1,11 +1,13 @@
 import Navbar from "./Navbar";
 import CarTile from "./CarTile";
+import RequestTile from "./RequestTile";
 import RentCarJSON from "../RentCar.json";
 import axios from "axios";
 import { useState } from "react";
 
 export default function Host() {
 const [data, updateData] = useState([]);
+const [requests, updateRequests] = useState([]);
 const [dataFetched, updateFetched] = useState(false);
 
 async function getMyCars() {
@@ -14,29 +16,65 @@ async function getMyCars() {
     const signer = provider.getSigner();
     let contract = new ethers.Contract(RentCarJSON.address, RentCarJSON.abi, signer)
     
-    let transaction = await contract.getMyCars()
+    try {
+        let myCarsTransaction = await contract.getMyCars()
+    
+        //Fetch all the details of every NFT from the contract and display
+        const myCars = await Promise.all(myCarsTransaction.map(async i => {
+            const tokenURI = await contract.tokenURI(i.tokenId);
+            let meta = await axios.get(tokenURI);
+            meta = meta.data;
+    
+            let price = ethers.utils.formatUnits(i.pricePerDay.toString(), 'ether');
+            
+            let item = {
+                tokenId: i.tokenId.toNumber(),
+                owner: i.owner.toString(),
+                price,
+                image: meta.image,
+                name: meta.name,
+                model: meta.model,
+                description: meta.description,
+            }
+            return item;
+        }))
 
-    //Fetch all the details of every NFT from the contract and display
-    const items = await Promise.all(transaction.map(async i => {
-        const tokenURI = await contract.tokenURI(i.tokenId);
-        let meta = await axios.get(tokenURI);
-        meta = meta.data;
+        updateData(myCars);
+    }
+    catch(e) {
+        console.log( "get myCars error"+e )
+    }
 
-        let price = ethers.utils.formatUnits(i.pricePerDay.toString(), 'ether');
+    try {
+        let myRequeststransaction = await contract.getRequestsForMyCars()
+    
+        //Fetch all the details of every NFT from the contract and display
+        const myRequests = await Promise.all(myRequeststransaction.map(async i => {
+            const tokenURI = await contract.tokenURI(i.tokenId);
+            let meta = await axios.get(tokenURI);
+            meta = meta.data;
+    
+            let totalPrice = ethers.utils.formatUnits(i.totalPrice.toString(), 'ether');
+            
+            let item = {
+                tokenId: i.tokenId.toNumber(),
+                renter: i.renter.toString(),
+                daysForRent: i.daysForRent.toString(),
+                totalPrice,
+                image: meta.image,
+                name: meta.name,
+                model: meta.model,
+            }
+            return item;
+        }))
         
-        let item = {
-            price,
-            tokenId: i.tokenId.toNumber(),
-            image: meta.image,
-            name: meta.name,
-            model: meta.model,
-            description: meta.description,
-        }
-        return item;
-    }))
+        updateRequests(myRequests);
+    }
+    catch(e) {
+        console.log( "get myRequests error"+e )
+    }
 
     updateFetched(true);
-    updateData(items);
 }
 
 if(!dataFetched)
@@ -45,18 +83,34 @@ if(!dataFetched)
 return (
     <div>
         <Navbar></Navbar>
-        <div className="flex flex-col place-items-center mt-20">
-            <div className="md:text-xl font-bold text-white">
-                My Cars
+        <div className="flex flex-row mt-20">
+            <div className="flex flex-col w-2/3 place-items-center">
+                <div className="md:text-xl font-bold text-white">
+                    My Cars
+                </div>
+                <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
+                    {(data != null && data.length  > 0)?                
+                        data.map((value, index) => {
+                            return <CarTile data={value} key={index}></CarTile>;
+                        })
+                        :
+                        <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">You dont have listed cars</div>
+                    }                
+                </div>
             </div>
-            <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
-                {(data != null && data.length  > 0)?                
-                    data.map((value, index) => {
-                        return <CarTile data={value} key={index}></CarTile>;
-                    })
-                    :
-                    <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">You dont have listed cars</div>
-                }                
+            <div className="flex flex-col w-1/3 place-items-center">
+                <div className="md:text-xl font-bold text-white">
+                    My requests
+                </div>
+                <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
+                    {(requests != null && requests.length  > 0)?                
+                        requests.map((value, index) => {
+                            return <RequestTile data={value} key={index}></RequestTile>;
+                        })
+                        :
+                        <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">You dont have requests</div>
+                    }                
+                </div>
             </div>
         </div>            
     </div>
---
+++ src/components/RequestTile.js
@@ -0,0 +1,71 @@
+import RentCarJSON from "../RentCar.json";
+
+function RequestTile (data) {
+
+    async function approveRentCar() {    
+        try {
+            const ethers = require("ethers");
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            let contract = new ethers.Contract(RentCarJSON.address, RentCarJSON.abi, signer)
+            
+            let transaction = await contract.approveRentCar(data.data.tokenId)
+            await transaction.wait()
+
+            alert("Car rent approved!");
+        }
+        catch(e) {
+            alert( "approveRentCar error"+e )
+        }
+    }
+
+    async function rejectRentCar() {    
+        try {
+            const ethers = require("ethers");
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            let contract = new ethers.Contract(RentCarJSON.address, RentCarJSON.abi, signer)
+            
+            let transaction = await contract.rejectRentCar(data.data.tokenId)
+            await transaction.wait()
+
+            alert("Car rent rejected!");
+        }
+        catch(e) {
+            alert( "rejectRentCar error"+e )
+        }
+    }
+
+    function formatAddress(address) {  
+        if (address == null || address.length < 10) 
+            return address;
+        return address.substr(0,5) + ".." + address.substr(address.length - 5);
+    }
+
+    return (
+        <div className="border-2 flex flex-col items-center rounded-md w-full shadow-2xl">
+            <div className="flex flex-row items-center mt-2 mx-4 w-full">
+                <img src={data.data.image} alt="" className="ml-4 w w-16 h-16 rounded-sm object-cover" />
+                <div className= "text-white w-full">
+                    <strong className="text-1">{data.data.name}</strong>
+                    <p className="display-inline">
+                        <strong className="text-sm">from {formatAddress(data.data.renter)}</strong>
+                        <strong className="text-sm"> for {data.data.daysForRent} day(s)</strong>
+                    </p>      
+                    <p className="display-inline">
+                    </p>  
+                    <p className="display-inline">
+                        <strong className="text-2">for {data.data.totalPrice} ether</strong>
+                    </p>     
+                </div>
+            </div>
+            <div className="flex flex-row items-center mt-2 mb-2">
+                    <button className="approveButton bg-blue-500 hover:bg-blue-700 text-white font-bold mx-5 py-2 px-4 rounded text-sm" onClick={() => approveRentCar()}>Approve</button>
+                    <button className="approveButton bg-red-500 hover:bg-red-700 text-white font-bold mx-5 py-2 px-4 rounded text-sm" onClick={() => rejectRentCar()}>Reject</button>
+                
+            </div>
+        </div>
+    )
+}
+
+export default RequestTile;
