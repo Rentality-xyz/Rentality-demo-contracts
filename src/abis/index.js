fe5e56a29ea0e8c2307061a8d59555c3f78551eb
Artem Islamov <artem.islamov@eigenmethod.com>
2023-04-07 14:21:49 +0100

    fix import  "./RentCar.localhost.json";  with lowercase

--- src/abis/index.js
+++ src/abis/index.js
@@ -1,5 +1,5 @@
 import RentCarJSONNet from "./RentCar.json";
-import RentCarJSONLocal from "./RentCar.Localhost.json";
+import RentCarJSONLocal from "./RentCar.localhost.json";
 
 const rentCarJSON = (process.env.REACT_APP_USE_LOCALHOST_BLOCKCHAIN)?.toLowerCase?.() === "true"
 ? RentCarJSONLocal
