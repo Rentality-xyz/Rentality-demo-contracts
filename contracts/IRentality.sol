fbeb0c1e1762a6862a3b16050c1f8bed842364e4
Artem Islamov <artem.islamov@eigenmethod.com>
2023-06-01 20:39:45 +0100

    update  IRentality interface with admin methods

--- contracts/IRentality.sol
+++ contracts/IRentality.sol
@@ -20,27 +20,42 @@ interface IRentality {
         uint8 ethToCurrencyDecimals;
     }
 
-    ///host functions 
-    function addCar(string memory tokenUri,
-        string memory carVinNumber,
-        uint256 pricePerDayInUsdCents,
-        uint256 tankVolumeInGal,
-        uint256 distanceIncludedInMi) external returns (uint);
-    function getCarMetadataURI(uint256 carId) external view returns (string memory);
-    function getMyCars() external view returns(RentalityCarToken.CarInfo[] memory);
-    function getTripsAsHost() external view returns (RentalityTripService.Trip[] memory);
-    function approveTripRequest(uint256 tripId) external;
-    function rejectTripRequest(uint256 tripId) external;
-    function checkInByHost(uint256 tripId, uint256 startFuelLevel, uint256 startOdometr) external;
-    function checkOutByHost(uint256 tripId,uint256 endFuelLevel,uint256 endOdometr) external;
-    function finishTrip(uint256 tripId) external;
-    function resolveIssue(uint256 tripId, uint256 fuelPricePerGal) external;
+    ///admin functions 
+        function owner() external view returns (address);
+        function getCarServiceAddress() external view returns (address);
+        function updateCarService(address contractAddress) external;
+        function getCurrencyConverterServiceAddress() external view returns (address);
+        function updateCurrencyConverterService(address contractAddress) external;
+        function getTripServiceAddress() external view returns (address);
+        function updateTripService(address contractAddress) external;
+        function getUserServiceAddress() external view returns (address);
+        function updateUserService(address contractAddress) external;
+        function withdrawFromPlatform(uint256 amount) external;
+        function withdrawAllFromPlatform() external;
+        function getPlatformFeeInPPM() external view returns (uint32);
+        function setPlatformFeeInPPM(uint32 valueInPPM) external;
 
-    ///guest functions 
-    function getAvailableCars() external view returns (RentalityCarToken.CarInfo[] memory);
-    function createTripRequest(CreateTripRequest memory request) external payable;
-    function getTripsAsGuest() external view returns (RentalityTripService.Trip[] memory);
-    function getCarsRentedByMe() external view returns(RentalityCarToken.CarInfo[] memory);
-    function checkInByGuest(uint256 tripId,uint256 startFuelLevel,uint256 startOdometr) external;
-    function checkOutByGuest(uint256 tripId,uint256 endFuelLevel,uint256 endOdometr) external;
+        ///host functions 
+        function addCar(string memory tokenUri,
+            string memory carVinNumber,
+            uint256 pricePerDayInUsdCents,
+            uint256 tankVolumeInGal,
+            uint256 distanceIncludedInMi) external returns (uint);
+        function getCarMetadataURI(uint256 carId) external view returns (string memory);
+        function getMyCars() external view returns(RentalityCarToken.CarInfo[] memory);
+        function getTripsAsHost() external view returns (RentalityTripService.Trip[] memory);
+        function approveTripRequest(uint256 tripId) external;
+        function rejectTripRequest(uint256 tripId) external;
+        function checkInByHost(uint256 tripId, uint256 startFuelLevel, uint256 startOdometr) external;
+        function checkOutByHost(uint256 tripId,uint256 endFuelLevel,uint256 endOdometr) external;
+        function finishTrip(uint256 tripId) external;
+        function resolveIssue(uint256 tripId, uint256 fuelPricePerGal) external;
+
+        ///guest functions 
+        function getAvailableCars() external view returns (RentalityCarToken.CarInfo[] memory);
+        function createTripRequest(CreateTripRequest memory request) external payable;
+        function getTripsAsGuest() external view returns (RentalityTripService.Trip[] memory);
+        function getCarsRentedByMe() external view returns(RentalityCarToken.CarInfo[] memory);
+        function checkInByGuest(uint256 tripId,uint256 startFuelLevel,uint256 startOdometr) external;
+        function checkOutByGuest(uint256 tripId,uint256 endFuelLevel,uint256 endOdometr) external;
 }
