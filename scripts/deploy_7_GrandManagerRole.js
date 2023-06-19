6c00a6b348867cd95d2c78407412083819546bcb
Artem Islamov <artem.islamov@eigenmethod.com>
2023-06-19 11:08:13 +0100

    RTY-78 Помилка при створені авто під новим користувачем

--- scripts/deploy_7_GrandManagerRole.js
+++ scripts/deploy_7_GrandManagerRole.js
@@ -1,4 +1,3 @@
-const saveJsonAbi = require("./utils/abiSaver");
 const RentalityUserServiceJSONNet = require("../src/abis/RentalityUserService.json");
 const RentalityUserServiceJSONLocal = require("../src/abis/RentalityUserService.localhost.json");
 const { ethers } = require("hardhat");
@@ -6,7 +5,6 @@ const addressesGanache = require("./ganacheAddresses.json")
 const addressesSepolia = require("./sepoliaAddresses.json")
 
 async function main() {
-  const contractName = "Rentality";
   const [deployer] = await ethers.getSigners();
   const balance = await deployer.getBalance();
   console.log(
@@ -23,54 +21,33 @@ async function main() {
   const addresses = chainId === 11155111 ? addressesSepolia : addressesGanache;
   const RentalityUserServiceJSON = chainId === 1337 ? RentalityUserServiceJSONLocal : RentalityUserServiceJSONNet;
 
-  const rentalityCarTokenAddress =  addresses.RentalityCarToken;
-  const rentalityCurrencyConverterAddress = addresses.RentalityCurrencyConverter;
-  const rentalityTripServiceAddress = addresses.RentalityTripService;
   const rentalityUserServiceAddress = addresses.RentalityUserService;
+  const rentalityAddress = addresses.Rentality;
 
-  if (!rentalityCarTokenAddress) {
-    console.log("rentalityCarTokenAddress is not set");
-    return;
-  }
-  if (!rentalityCurrencyConverterAddress) {
-    console.log("rentalityCurrencyConverterAddress is not set");
-    return;
-  }
-  if (!rentalityTripServiceAddress) {
-    console.log("rentalityTripServiceAddress is not set");
-    return;
-  }
   if (!rentalityUserServiceAddress) {
     console.log("rentalityUserServiceAddress is not set");
     return;
   }
+  if (!rentalityAddress) {
+    console.log("rentalityAddress is not set");
+    return;
+  }
 
-  console.log("rentalityCarTokenAddress is:", rentalityCarTokenAddress);
-  console.log(
-    "rentalityCurrencyConverterAddress is:",
-    rentalityCurrencyConverterAddress
-  );
-  console.log("rentalityTripServiceAddress is:", rentalityTripServiceAddress);
   console.log("rentalityUserServiceAddress is:", rentalityUserServiceAddress);
+  console.log("rentalityAddress is:", rentalityAddress);
   
-  const contractFactory = await ethers.getContractFactory(contractName);
-  const contract = await contractFactory.deploy(
-    rentalityCarTokenAddress,
-    rentalityCurrencyConverterAddress,
-    rentalityTripServiceAddress,
-    rentalityUserServiceAddress);
-  await contract.deployed();
-  console.log(contractName + " deployed to:", contract.address);
-
   let rentalityUserServiceContract = new ethers.Contract(
-    RentalityUserServiceJSON.address,
+    rentalityUserServiceAddress,
     RentalityUserServiceJSON.abi,
     deployer
   );
-  await rentalityUserServiceContract.grantManagerRole(contract.address);
+  try{
+    await rentalityUserServiceContract.grantManagerRole(rentalityAddress);
+    console.log("manager role granded");
+  } catch(e){
+    console.log("grand manager role error:", e);
+  }
   //await rentalityUserServiceContract.connect(deployer).grantManagerRole(contract.address);
-  
-  saveJsonAbi(contractName, chainId, contract);
 }
 
 main()
