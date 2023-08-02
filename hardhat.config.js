294d2c22c92aeb166a23cb789e2da87df0077b72
MantasM <mantasm.dev@gmail.com>
2023-08-02 20:25:30 +0300

    fix: uncomment config

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,23 +13,23 @@ module.exports = {
     localhost: {
       chainId: 1337,
     },
-    // ganache: {
-    //   url: process.env.URL_LOCALHOST_GANACHE,
-    //   accounts: [process.env.GANACHE_PRIVATE_KEY],
-    //   chainId: 1337,
-    // },
-    // goerli: {
-    //   url: process.env.ALCHEMY_API_URL_GOERLI,
-    //   accounts: [process.env.PRIVATE_KEY],
-    // },
-    // mumbai: {
-    //   url: process.env.ALCHEMY_API_URL_MUMBAI,
-    //   accounts: [process.env.PRIVATE_KEY],
-    // },
-    // sepolia: {
-    //   url: process.env.ALCHEMY_API_URL_SEPOLIA,
-    //   accounts: [process.env.PRIVATE_KEY],
-    // },
+    ganache: {
+      url: process.env.URL_LOCALHOST_GANACHE,
+      accounts: [process.env.GANACHE_PRIVATE_KEY],
+      chainId: 1337,
+    },
+    goerli: {
+      url: process.env.ALCHEMY_API_URL_GOERLI,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mumbai: {
+      url: process.env.ALCHEMY_API_URL_MUMBAI,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    sepolia: {
+      url: process.env.ALCHEMY_API_URL_SEPOLIA,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
   solidity: {
     version: '0.8.17',
