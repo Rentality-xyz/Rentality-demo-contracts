409544f015a991ca3f08c41c405060893e9af733
protasov <protasovgleb290@gmail.com>
2023-11-27 16:56:51 +0200

    comments

--- contracts/RentalityUtils.sol
+++ contracts/RentalityUtils.sol
@@ -8,10 +8,23 @@ import "./IRentalityGateway.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import './RentalityGeoService.sol';
 
+/// @title RentalityUtils Library
+/// @notice
+/// This library provides utility functions for handling coordinates, string manipulation,
+/// and parsing responses related to geolocation data. It includes functions for checking
+/// coordinates within a specified bounding box, parsing strings, converting between data types,
+/// and URL encoding. The library is used in conjunction with other Rentality contracts.
 library RentalityUtils {
-
+    // Constant multiplier for converting decimal coordinates to integers
     uint256 constant multiplier = 10**7;
-
+    /// @notice Checks if a set of coordinates falls within a specified bounding box.
+    /// @param locationLat Latitude of the location to check.
+    /// @param locationLng Longitude of the location to check.
+    /// @param northeastLat Latitude of the northeast corner of the bounding box.
+    /// @param northeastLng Longitude of the northeast corner of the bounding box.
+    /// @param southwestLat Latitude of the southwest corner of the bounding box.
+    /// @param southwestLng Longitude of the southwest corner of the bounding box.
+    /// @return Returns true if the coordinates are within the bounding box, false otherwise.
     function checkCoordinates(
         string memory locationLat,
         string memory locationLng,
@@ -29,8 +42,9 @@ library RentalityUtils {
 
         return (lat >= swLat && lat <= neLat && lng >= swLng && lng <= neLng);
     }
-
-    
+    /// @notice Parses an integer from a string.
+    /// @param _a The input string to parse.
+    /// @return Returns the parsed integer value.
     function parseInt(string memory _a) internal pure returns (int256) {
         bytes memory bresult = bytes(_a);
         int256 mint = 0;
@@ -39,42 +53,46 @@ library RentalityUtils {
             if ((uint8(bresult[i]) >= 48) && (uint8(bresult[i]) <= 57)) {
                 if (decimals) {
                     if (i - 1 - indexOf(bresult, ".") > 6) break;
-                    mint = mint * 10 + int256(uint256(uint8(bresult[i])) - 48); 
+                    mint = mint * 10 + int256(uint256(uint8(bresult[i])) - 48);
                 } else {
-                    mint = mint * 10 + int256(uint256(uint8(bresult[i])) - 48); 
+                    mint = mint * 10 + int256(uint256(uint8(bresult[i])) - 48);
                 }
             } else if (uint8(bresult[i]) == 46) decimals = true;
         }
         if (indexOf(bresult, "-") == 0) {
-            return -mint * int256(multiplier); 
+            return - mint * int256(multiplier);
         }
-        return mint * int256(multiplier); 
+        return mint * int256(multiplier);
     }
-
-
+    /// @notice Finds the index of a substring in a given string.
+    /// @param haystack The string to search within.
+    /// @param needle The substring to search for.
+    /// @return Returns the index of the first occurrence of the substring, or the length of the string if not found.
     function indexOf(bytes memory haystack, string memory needle) internal pure returns (uint) {
         bytes memory bneedle = bytes(needle);
-        if(bneedle.length > haystack.length) {
+        if (bneedle.length > haystack.length) {
             return haystack.length;
         }
 
         bool found = false;
         uint i;
-        for(i = 0; i <= haystack.length - bneedle.length; i++) {
+        for (i = 0; i <= haystack.length - bneedle.length; i++) {
             found = true;
-            for(uint j = 0; j < bneedle.length; j++) {
-                if(haystack[i+j] != bneedle[j]) {
+            for (uint j = 0; j < bneedle.length; j++) {
+                if (haystack[i + j] != bneedle[j]) {
                     found = false;
                     break;
                 }
             }
-            if(found) {
+            if (found) {
                 break;
             }
         }
         return i;
     }
-
+    /// @notice Converts a string to lowercase.
+    /// @param str The input string to convert.
+    /// @return Returns the lowercase version of the input string.
     function toLower(string memory str) internal pure returns (string memory) {
         bytes memory bStr = bytes(str);
         bytes memory bLower = new bytes(bStr.length);
@@ -89,15 +107,14 @@ library RentalityUtils {
         }
         return string(bLower);
     }
-
-    function containWord(
-        string memory where,
-        string memory what
-    ) internal pure returns (bool found) {
+    /// @notice Checks if a string contains a specific word.
+    /// @param where The string to search within.
+    /// @param what The word to search for.
+    /// @return found Returns true if the word is found, false otherwise.
+    function containWord(string memory where, string memory what) internal pure returns (bool found) {
         bytes memory whatBytes = bytes(what);
         bytes memory whereBytes = bytes(where);
 
-        //require(whereBytes.length >= whatBytes.length);
         if (whereBytes.length < whatBytes.length) {
             return false;
         }
@@ -117,18 +134,25 @@ library RentalityUtils {
         }
         return found;
     }
-
-    function getHashFromString(
-        string memory str
-    ) internal pure returns (bytes32) {
+    /// @notice Generates a hash from a string.
+    /// @param str The input string to hash.
+    /// @return Returns the keccak256 hash of the input string.
+    function getHashFromString(string memory str) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(str));
     }
-
+    /// @notice Calculates the ceiling of the division of two numbers.
+    /// @param startDateTime The numerator of the division.
+    /// @param endDateTime The denominator of the division.
+    /// @return Returns the result of the division rounded up to the nearest whole number.
     function getCeilDays(uint64 startDateTime, uint64 endDateTime) public pure returns (uint64) {
         uint64 duration = endDateTime - startDateTime;
         return uint64(Math.ceilDiv(duration, 1 days));
     }
-
+    /// @notice Populates an array of chat information using data from trips, user service, and car service.
+    /// @param trips Array of RentalityTripService.Trip structures.
+    /// @param userService RentalityUserService contract instance.
+    /// @param carService RentalityCarToken contract instance.
+    /// @return chatInfoList Array of IRentalityGateway.ChatInfo structures.
     function populateChatInfo(
         RentalityTripService.Trip[] memory trips,
         RentalityUserService userService,
@@ -158,7 +182,9 @@ library RentalityUtils {
 
         return chatInfoList;
     }
-
+    /// @notice Parses a response string containing geolocation data.
+    /// @param response The response string to parse.
+    /// @return result Parsed geolocation data in RentalityGeoService.ParsedGeolocationData structure.
     function parseResponse(string memory response) public pure returns (RentalityGeoService.ParsedGeolocationData memory) {
         RentalityGeoService.ParsedGeolocationData memory result;
 
@@ -192,7 +218,9 @@ library RentalityUtils {
 
         return result;
     }
-
+    /// @notice Splits a string into an array of substrings based on a delimiter.
+    /// @param input The input string to split.
+    /// @return parts Array of substrings.
     function splitString(string memory input) internal pure returns (string[] memory) {
         bytes memory inputBytes = bytes(input);
         bytes memory delimiterBytes = bytes("|");
@@ -226,7 +254,9 @@ library RentalityUtils {
 
         return parts;
     }
-
+    /// @notice Splits a key-value pair string into an array of key and value.
+    /// @param input The input string to split.
+    /// @return parts Array containing key and value.
     function splitKeyValue(string memory input) internal pure returns (string[] memory) {
         bytes memory inputBytes = bytes(input);
         bytes memory delimiterBytes = bytes("^");
@@ -258,11 +288,16 @@ library RentalityUtils {
 
         return parts;
     }
-
+    /// @notice Compares two strings for equality.
+    /// @param a The first string.
+    /// @param b The second string.
+    /// @return Returns true if the strings are equal, false otherwise.
     function compareStrings(string memory a, string memory b) internal pure returns (bool) {
         return (keccak256(bytes(a)) == keccak256(bytes(b)));
     }
-
+    /// @notice URL encodes a string.
+    /// @param input The input string to encode.
+    /// @return output The URL-encoded string.
     function urlEncode(string memory input) internal pure returns (string memory) {
         bytes memory inputBytes = bytes(input);
         string memory output = "";
@@ -278,7 +313,7 @@ library RentalityUtils {
                         bytes1(uint8(inputBytes[i]) % 16 + 48)
                     )
                 );
-            } 
+            }
             else {
                 output = string(
                     abi.encodePacked(
@@ -290,6 +325,10 @@ library RentalityUtils {
         }
         return output;
     }
+
+    /// @dev Converts a bytes32 data to a bytes array.
+    /// @param _data The input bytes32 data to convert.
+    /// @return Returns the packed representation of the input data as a bytes array.
     function toBytes(bytes32 _data) public pure returns (bytes memory) {
         return abi.encodePacked(_data);
     }
