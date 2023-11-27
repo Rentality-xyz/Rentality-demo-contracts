7586c2704821fd77646d4a93b93bbdb9900c5f38
protasov <protasovgleb290@gmail.com>
2023-11-27 16:56:51 +0200

    comments

--- contracts/EthToUsdConverter.sol
+++ contracts/EthToUsdConverter.sol
@@ -3,41 +3,55 @@ pragma solidity ^0.8.9;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract EthToUsdConverter { 
-    
-    AggregatorV3Interface internal ethToUsdPriceFeed;
-    int256 private currentEthToUsdPrice; 
+/// @title EthToUsdConverter
+/// @dev A contract for converting Ethereum to USD using Chainlink price feeds.
+contract EthToUsdConverter {
 
+    AggregatorV3Interface internal ethToUsdPriceFeed;
+    int256 private currentEthToUsdPrice;
+
+    /// @dev Constructor function to initialize the contract with the specified Chainlink ETH/USD price feed address.
+    /// @param ethToUsdPriceFeedAddress The address of the Chainlink ETH/USD price feed.
     constructor(address ethToUsdPriceFeedAddress) {
 
-        //ETH/USD (Goerli Testnet) 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        //ETH/USD (Mumbai Testnet) 0x0715A7794a1dc8e42615F059dD6e406A6594651A
-        //ETH/USD (Sepolia Testnet) 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ETH/USD (Goerli Testnet) 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        // ETH/USD (Mumbai Testnet) 0x0715A7794a1dc8e42615F059dD6e406A6594651A
+        // ETH/USD (Sepolia Testnet) 0x694AA1769357215DE4FAC081bf1f309aDC325306
         ethToUsdPriceFeed = AggregatorV3Interface(ethToUsdPriceFeedAddress);
         currentEthToUsdPrice = getLatestEthToUsdPrice();
-    }  
-    
+    }
+
+    /// @dev Get the latest ETH to USD price from the Chainlink price feed.
+    /// @return int256 The latest ETH to USD price
     function getLatestEthToUsdPrice() public view returns (int256) {
-         (
-            /*uint80 roundID*/,
+        (
+        /*uint80 roundID*/,
             int price,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
+        /*uint startedAt*/,
+        /*uint timeStamp*/,
+        /*uint80 answeredInRound*/
         ) = ethToUsdPriceFeed.latestRoundData();
 
-        return price; //  example price returned 165110000000
+        return price; // example price returned: 165110000000
     }
 
+    /// @dev Get the decimals of the price feed.
+    /// @return uint8 The decimals of the price feed.
     function getPriceFeedDecimals() public view returns (uint8) {
-        return ethToUsdPriceFeed.decimals(); 
-    }
-    
-    function getEthFromUsd(uint256 valueInUsdCents) public view returns (uint256) {
-        return (valueInUsdCents * (1 ether) * (10 ** (getPriceFeedDecimals()-2))) / uint(getLatestEthToUsdPrice());
+        return ethToUsdPriceFeed.decimals();
     }
 
+    /// @dev Convert a given USD value to ETH.
+    /// @param valueInUsdCents The value in USD cents to convert to ETH.
+    /// @return uint256 The equivalent amount in ETH.
+    function getEthFromUsd(uint256 valueInUsdCents) public view returns (uint256) {
+        return (valueInUsdCents * (1 ether) * (10 ** (getPriceFeedDecimals() - 2))) / uint(getLatestEthToUsdPrice());
+    }
+
+    /// @dev Convert a given ETH value to USD.
+    /// @param valueInEth to convert to USD.
+    /// @return uint256 The equivalent amount in USD.
     function getUsdFromEth(uint256 valueInEth) public view returns (uint256) {
-        return (valueInEth * uint(getLatestEthToUsdPrice()) / ((10 ** (getPriceFeedDecimals()-2)) * (1 ether)));
+        return (valueInEth * uint(getLatestEthToUsdPrice()) / ((10 ** (getPriceFeedDecimals() - 2)) * (1 ether)));
     }
 }
--- contracts/RentalityChatHelper.sol
+++ contracts/RentalityChatHelper.sol
@@ -1,28 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+
+ /// @title RentalityChatHelper
+ /// @notice A contract to manage chat key pairs for users
+ /// @dev Users can set and retrieve their chat key pairs, and get public keys of specified addresses.
 contract RentalityChatHelper {
+     /// @dev Struct to represent a pair of private and public chat keys
     struct ChatKeyPair {
         string privateKey;
         string publicKey;
     }
 
+     /// @dev Struct to associate an Ethereum address with a public chat key
     struct AddressPublicKey {
         address userAddress;
         string publicKey;
     }
 
-
+    // Mapping to store chat key pairs associated with Ethereum addresses
     mapping(address => ChatKeyPair) private addressToChatKeyPair;
 
+     /// @notice Set the chat key pair for the calling user
+     /// @param chatPrivateKey The private chat key of the user
+     /// @param chatPublicKey The public chat key of the user
     function setMyChatPublicKey(string memory chatPrivateKey, string memory chatPublicKey) public {
         addressToChatKeyPair[tx.origin] = ChatKeyPair(chatPrivateKey, chatPublicKey);
     }
 
+     /// @notice Get the chat key pair of the calling user
+     /// @return The private and public chat keys of the calling user
     function getMyChatKeys() public view returns (string memory, string memory) {
         return (addressToChatKeyPair[tx.origin].privateKey, addressToChatKeyPair[tx.origin].publicKey);
     }
 
+     /// @notice Get the public chat keys associated with specified addresses
+     /// @param addresses An array of Ethereum addresses
+     /// @return An array of AddressPublicKey structs containing user addresses and their public chat keys
     function getChatPublicKeys(address[] memory addresses) public view returns (AddressPublicKey[] memory) {
         AddressPublicKey[] memory result = new AddressPublicKey[](addresses.length);
 
