9fef3ad0293e8970295544f2d560af1fff6d068c
protasov <protasovgleb290@gmail.com>
2023-12-15 11:51:09 +0200

    test + comments

--- contracts/engine/RentalityEnginesService.sol
+++ contracts/engine/RentalityEnginesService.sol
@@ -4,17 +4,20 @@ pragma solidity ^0.8.9;
 import "./ARentalityEngine.sol";
 import "../RentalityUserService.sol";
 
-contract RentalityEnginesService
-{
+/// @title RentalityEnginesService - Manages different types of Rentality engines.
+/// @notice This contract allows the addition, update, and interaction with various Rentality engines.
+contract RentalityEnginesService {
     RentalityUserService private userService;
     uint8 private eTypeCounter = 1;
     mapping(uint8 => ARentalityEngine) private engineTypeToEngineContract;
 
     error Overflow();
 
+    /// @notice Constructor to initialize the RentalityEnginesService contract.
+    /// @param _userService The address of the RentalityUserService contract.
+    /// @param engineServices An array of addresses representing existing engine contracts.
     constructor(address _userService, address[] memory engineServices) {
-        if (engineServices.length >= type(uint8).max - 1)
-        {
+        if (engineServices.length >= type(uint8).max - 1) {
             revert Overflow();
         }
 
@@ -25,19 +28,23 @@ contract RentalityEnginesService
         }
         userService = RentalityUserService(_userService);
     }
-    modifier onlyAdmin()
-    {
-        require(userService.isAdmin(msg.sender),"Only for Admin.");
+
+    /// @notice Modifier to restrict access to only administrators.
+    modifier onlyAdmin() {
+        require(userService.isAdmin(msg.sender), "Only for Admin.");
         _;
     }
-    modifier onlyManager()
-    {
-        require(userService.isManager(msg.sender),"Only for Manager.");
+
+    /// @notice Modifier to restrict access to only managers.
+    modifier onlyManager() {
+        require(userService.isManager(msg.sender), "Only for Manager.");
         _;
     }
+
+    /// @notice Adds a new engine service contract to the system.
+    /// @param engineService The address of the new engine service contract.
     function addEngineService(address engineService) public onlyAdmin {
-        if (eTypeCounter == type(uint8).max - 1)
-        {
+        if (eTypeCounter == type(uint8).max - 1) {
             revert Overflow();
         }
         engineTypeToEngineContract[eTypeCounter] = ARentalityEngine(engineService);
@@ -45,59 +52,98 @@ contract RentalityEnginesService
         eTypeCounter += 1;
     }
 
-    function updateEngineService(address engineService, uint8 eType) public onlyAdmin{
+    /// @notice Updates an existing engine service contract in the system.
+    /// @param engineService The address of the updated engine service contract.
+    /// @param eType The engine type associated with the contract.
+    function updateEngineService(address engineService, uint8 eType) public onlyAdmin {
         engineTypeToEngineContract[eType] = ARentalityEngine(engineService);
     }
 
+    /// @notice Retrieves the address of an engine contract based on its engine type.
+    /// @param eType The engine type for which to retrieve the address.
+    /// @return The address of the specified engine contract.
     function getEngineAddressById(uint8 eType) public view returns (address) {
-        return address (engineTypeToEngineContract[eType]);
+        return address(engineTypeToEngineContract[eType]);
     }
 
-
-    function addCar(uint256 carId, uint8 eType, uint64[] memory params) public onlyManager
-    {
+    /// @notice Adds a new car to the system using a specific engine type.
+    /// @param carId The unique identifier of the car.
+    /// @param eType The engine type associated with the car.
+    /// @param params An array of parameters required for adding the car.
+    function addCar(uint256 carId, uint8 eType, uint64[] memory params) public onlyManager {
         engineTypeToEngineContract[eType].addCar(carId, params);
     }
 
-    function updateCar(uint256 carId, uint8 eType, uint64[] memory params) public onlyManager
-    {
+    /// @notice Updates an existing car in the system using a specific engine type.
+    /// @param carId The unique identifier of the car.
+    /// @param eType The engine type associated with the car.
+    /// @param params An array of parameters required for updating the car.
+    function updateCar(uint256 carId, uint8 eType, uint64[] memory params) public onlyManager {
         engineTypeToEngineContract[eType].updateCar(carId, params);
     }
 
-    function burnCar(uint256 carId, uint8 eType) public onlyManager
-    {
+    /// @notice Removes a car from the system using a specific engine type.
+    /// @param carId The unique identifier of the car.
+    /// @param eType The engine type associated with the car.
+    function burnCar(uint256 carId, uint8 eType) public onlyManager {
         engineTypeToEngineContract[eType].burnCar(carId);
     }
 
+    /// @notice Verifies resource prices for a specific engine type.
+    /// @param prices An array of uint64 values representing resource prices.
+    /// @param eType The engine type for which to verify the resource prices.
     function verifyResourcePrice(uint64[] memory prices, uint8 eType) public view {
         engineTypeToEngineContract[eType].verifyResourcePrice(prices);
     }
 
+    /// @notice Verifies the start parameters for a specific engine type.
+    /// @param params An array of uint64 values representing start parameters.
+    /// @param eType The engine type for which to verify the start parameters.
     function verifyStartParams(uint64[] memory params, uint8 eType) public {
         engineTypeToEngineContract[eType].verifyStartParams(params);
     }
 
-    function verifyEndParams(uint64[] memory startParams, uint64[] memory endParams, uint8 eType) public
-    {
+    /// @notice Verifies the end parameters for a specific engine type.
+    /// @param startParams An array of uint64 values representing start parameters.
+    /// @param endParams An array of uint64 values representing end parameters.
+    /// @param eType The engine type for which to verify the end parameters.
+    function verifyEndParams(uint64[] memory startParams, uint64[] memory endParams, uint8 eType) public {
         engineTypeToEngineContract[eType].verifyEndParams(startParams, endParams);
-
     }
-    function compareParams(uint64[] memory startParams, uint64[] memory endParams, uint8 eType) public view
-    {
+
+    /// @notice Compares parameters for a specific engine type.
+    /// @param startParams An array of uint64 values representing start parameters.
+    /// @param endParams An array of uint64 values representing end parameters.
+    /// @param eType The engine type for which to compare the parameters.
+    function compareParams(uint64[] memory startParams, uint64[] memory endParams, uint8 eType) public view {
         engineTypeToEngineContract[eType].compareParams(startParams, endParams);
-
     }
 
+    /// @notice Retrieves the number of parameters expected by the panel for a specific engine type.
+    /// @param eType The engine type for which to get the panel parameters amount.
+    /// @return The number of parameters expected by the panel.
     function getPanelParamsAmount(uint8 eType) public returns (uint256) {
-       return engineTypeToEngineContract[eType].getParamsAmount();
-
+        return engineTypeToEngineContract[eType].getParamsAmount();
     }
 
+    /// @notice Computes extra costs for a specific engine type.
+    /// @param eType The engine type for which to compute extra costs.
+    /// @param params An array of uint64 values representing parameters.
+    /// @return The computed extra costs in USD cents.
     function extraCosts(uint8 eType, uint64[] memory params) public returns (uint64) {
         return engineTypeToEngineContract[eType].extraCosts(params);
     }
 
-
+    /// @notice Computes the resolve amount in USD cents for a specific engine type and car rental.
+    /// @param engineType The engine type associated with the car.
+    /// @param fuelPrices An array of uint64 values representing fuel prices.
+    /// @param startParams An array of uint64 values representing the initial parameters of the rental.
+    /// @param endParams An array of uint64 values representing the final parameters of the rental.
+    /// @param carId The unique identifier of the car.
+    /// @param milesIncludedPerDay The number of miles included per day in the rental.
+    /// @param pricePerDayInUsdCents The rental price per day in USD cents.
+    /// @param tripDays The total number of days in the rental trip.
+    /// @return The total resolve amount and the fuel-specific resolve amount in USD cents.
     function getResolveAmountInUsdCents(
         uint8 engineType,
         uint64[] memory fuelPrices,
@@ -106,17 +152,15 @@ contract RentalityEnginesService
         uint256 carId,
         uint64 milesIncludedPerDay,
         uint64 pricePerDayInUsdCents,
-        uint64 tripDays) public returns (uint64, uint64)
-    {
-       return engineTypeToEngineContract[engineType].getResolveAmountInUsdCents(
-        fuelPrices,
-        startParams,
-        endParams,
-        carId,
-        milesIncludedPerDay,
-        pricePerDayInUsdCents,
-        tripDays);
+        uint64 tripDays
+    ) public returns (uint64, uint64) {
+        return engineTypeToEngineContract[engineType].getResolveAmountInUsdCents(
+            fuelPrices,
+            startParams,
+            endParams,
+            carId,
+            milesIncludedPerDay,
+            pricePerDayInUsdCents,
+            tripDays);
     }
-
-
 }
--- contracts/engine/RentalityPatrolEngine.sol
+++ contracts/engine/RentalityPatrolEngine.sol
@@ -1,31 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-
 import "./ARentalityEngine.sol";
 
-contract RentalityPatrolEngine is ARentalityEngine
-{
+/// @title RentalityPatrolEngine - Implementation of a patrol engine in the Rentality system.
+/// @notice This contract extends ARentalityEngine and adds functionality specific to patrol engines.
+contract RentalityPatrolEngine is ARentalityEngine {
 
-    struct PatrolEngine
-    {
+    // Struct to store patrol engine details.
+    struct PatrolEngine {
         uint64 tankVolumeInGal;
         uint64 fuelPricePerGalInUsdCents;
     }
 
+    // Mapping from car ID to patrol engine details.
     mapping(uint256 => PatrolEngine) private carIdToPatrolEngine;
 
-    constructor(address _userService)
-    {
+    /// @notice Constructor to set the RentalityUserService address during deployment.
+    constructor(address _userService) {
         userService = RentalityUserService(_userService);
     }
+
+    /// @notice Sets the engine type. Only callable by an admin.
+    /// @param _eType The new engine type to be set.
     function setEType(uint8 _eType) public override {
         require(userService.isAdmin(tx.origin), "only Admin.");
         eType = _eType;
     }
 
-    function addCar(uint256 carId, uint64[] memory params) public override onlyManager
-    {
+    /// @notice Adds a new patrol car to the system with specified tank volume and fuel price.
+    /// @param carId The unique identifier of the patrol car.
+    /// @param params An array of two uint64 values representing tank volume and fuel price.
+    function addCar(uint256 carId, uint64[] memory params) public override onlyManager {
         isCorrectArgs((params[0] != 0 && params[1] != 0));
 
         PatrolEngine storage engine = carIdToPatrolEngine[carId];
@@ -33,27 +39,42 @@ contract RentalityPatrolEngine is ARentalityEngine
         engine.fuelPricePerGalInUsdCents = params[1];
     }
 
-// @param params[0] fuelPricePerGalInUsdCents
-    function updateCar(uint256 carId, uint64[] memory params) public override onlyManager
-    {
+    /// @notice Updates the fuel price for an existing patrol car in the system.
+    /// @param carId The unique identifier of the patrol car.
+    /// @param params An array containing the new fuel price.
+    /// @param params[0] The new fuel price per gallon in USD cents.
+    function updateCar(uint256 carId, uint64[] memory params) public override onlyManager {
         isCorrectArgs(params[0] != 0);
         carIdToPatrolEngine[carId].fuelPricePerGalInUsdCents = params[0];
-
     }
 
-    function burnCar(uint256 carId) public override onlyManager
-    {
+    /// @notice Removes a patrol car from the system.
+    /// @param carId The unique identifier of the patrol car to be removed.
+    function burnCar(uint256 carId) public override onlyManager {
         delete carIdToPatrolEngine[carId];
     }
 
+    /// @notice Returns zero extra costs for patrol cars.
+    /// @param params An array of uint64 values representing parameters (not used for patrol engines).
     function extraCosts(uint64[] memory params) public pure override returns (uint64) {
         return 0;
     }
 
-    function getEngineData(uint256 carId) public view returns(PatrolEngine memory) {
+    /// @notice Retrieves patrol engine details for a specific patrol car.
+    /// @param carId The unique identifier of the patrol car.
+    function getEngineData(uint256 carId) public view returns (PatrolEngine memory) {
         return carIdToPatrolEngine[carId];
     }
 
+    /// @notice Calculates the resolve amount in USD cents for a patrol car rental.
+    /// @param fuelPrices An array of uint64 values representing fuel prices (not used for patrol engines).
+    /// @param startParams An array of uint64 values representing the initial parameters of the rental.
+    /// @param endParams An array of uint64 values representing the final parameters of the rental.
+    /// @param carId The unique identifier of the patrol car.
+    /// @param milesIncludedPerDay The number of miles included per day in the rental.
+    /// @param pricePerDayInUsdCents The rental price per day in USD cents.
+    /// @param tripDays The total number of days in the rental trip.
+    /// @return The total resolve amount and the fuel-specific resolve amount in USD cents.
     function getResolveAmountInUsdCents(
         uint64[] memory fuelPrices,
         uint64[] memory startParams,
@@ -61,8 +82,8 @@ contract RentalityPatrolEngine is ARentalityEngine
         uint256 carId,
         uint64 milesIncludedPerDay,
         uint64 pricePerDayInUsdCents,
-        uint64 tripDays) public view override returns (uint64, uint64)
-    {
+        uint64 tripDays
+    ) public view override returns (uint64, uint64) {
         return (
             getDrivenMilesResolveAmountInUsdCents(
             startParams[1],
@@ -71,15 +92,21 @@ contract RentalityPatrolEngine is ARentalityEngine
             pricePerDayInUsdCents,
             tripDays
         ),
-
             getFuelResolveAmountInUsdCents(
             endParams[0],
             startParams[0],
             carIdToPatrolEngine[carId].tankVolumeInGal,
             fuelPrices[0]
-        ));
+        )
+        );
     }
 
+    /// @notice Calculates the resolve amount in USD cents based on fuel consumption for a patrol car.
+    /// @param endFuelLevelInPercents The final fuel level of the patrol car in percentages.
+    /// @param startFuelLevelInPercents The initial fuel level of the patrol car in percentages.
+    /// @param tankVolume The tank volume of the patrol car in gallons.
+    /// @param fuelPricePerGalInUsdCents The fuel price per gallon in USD cents.
+    /// @return The fuel-specific resolve amount in USD cents.
     function getFuelResolveAmountInUsdCents(
         uint64 endFuelLevelInPercents,
         uint64 startFuelLevelInPercents,
@@ -88,9 +115,8 @@ contract RentalityPatrolEngine is ARentalityEngine
     ) public pure returns (uint64) {
         if (endFuelLevelInPercents >= startFuelLevelInPercents) return 0;
 
-        return
+        return (
             (((startFuelLevelInPercents - endFuelLevelInPercents) * tankVolume ) * 1000 / 100)
-            * fuelPricePerGalInUsdCents / 1000;
+            * fuelPricePerGalInUsdCents / 1000);
     }
-
 }
