8e2581822882e98940838d587e9bf0a463a2396c
protasov <protasovgleb290@gmail.com>
2024-01-04 16:57:06 +0200

    transfer payments info to payment service

--- contracts/RentalityPaymentService.sol
+++ contracts/RentalityPaymentService.sol
@@ -5,13 +5,48 @@ pragma solidity ^0.8.9;
 
 import "./IRentalityAccessControl.sol";
 import "./proxy/UUPSOwnable.sol";
+import "./RentalityTripService.sol";
 
 /// @title Rentality Payment Service Contract
 /// @notice This contract manages platform fees and allows the adjustment of the platform fee by the manager.
 /// @dev It is connected to RentalityUserService to check if the caller is an admin.
-contract RentalityPaymentService is UUPSOwnable{
+contract RentalityPaymentService is UUPSOwnable {
     uint32 platformFeeInPPM;
     IRentalityAccessControl private userService;
+
+    /// @dev Enumeration representing the currency type used for payments.
+    enum CurrencyType {
+        ETH
+    }
+
+    /// @dev Struct containing payment information for a trip.
+    struct PaymentInfo {
+        uint256 tripId;
+        address from;
+        address to;
+        uint64 totalDayPriceInUsdCents;
+        uint64 taxPriceInUsdCents;
+        uint64 depositInUsdCents;
+        uint64 resolveAmountInUsdCents;
+        CurrencyType currencyType;
+        int256 ethToCurrencyRate;
+        uint8 ethToCurrencyDecimals;
+        uint64 resolveFuelAmountInUsdCents;
+        uint64 resolveMilesAmountInUsdCents;
+}
+
+    // Struct to store transaction history details for a trip
+    struct TransactionInfo {
+        uint256 rentalityFee;
+        uint256 depositRefund;
+        // Earnings from the trip (cancellation or completion)
+        uint256 tripEarnings;
+        // Timestamp of the transaction
+        uint256 dateTime;
+        // Status before trip cancellation, will be 'Finished' in case of completed trip.
+        RentalityTripService.TripStatus statusBeforeCancellation;
+    }
+
 
 
     /// @notice Get the current platform fee in parts per million (PPM).
@@ -31,14 +66,12 @@ contract RentalityPaymentService is UUPSOwnable{
         platformFeeInPPM = valueInPPM;
     }
 
-
     /// @notice Get the platform fee from a given value.
     /// @param value The value from which to calculate the platform fee.
     /// @return The platform fee calculated from the given value.
     function getPlatformFeeFrom(uint256 value) public view returns (uint256) {
         return (value * platformFeeInPPM) / 1_000_000;
     }
-
 
     /// @notice Constructor to initialize the RentalityPaymentService.
     /// @param _userService The address of the RentalityUserService contract
