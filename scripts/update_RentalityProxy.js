d1644f7bc8db5c144b9f36d583a92a2272f0ff7d
protasov <protasovgleb290@gmail.com>
2024-01-30 18:53:30 +0200

    fix

--- scripts/update_RentalityProxy.js
+++ scripts/update_RentalityProxy.js
@@ -8,11 +8,12 @@ async function main() {
   const balance = await ethers.provider.getBalance(deployer.address)
   console.log('Deployer address is:', await deployer.getAddress(), ' with balance:', balance)
 
+  const chainId = (await deployer.provider?.getNetwork())?.chainId ?? -1
   const contractName = readlineSync.question('Enter contract name to update:\n')
 
   console.log('Contract name: ', contractName)
 
-  const contractAddress = readFromFile(contractName)
+  const contractAddress = readFromFile(contractName, chainId)
 
   if (contractAddress === null) {
     console.log(`Contract address ${contractName}: not found. Finishing... `)
