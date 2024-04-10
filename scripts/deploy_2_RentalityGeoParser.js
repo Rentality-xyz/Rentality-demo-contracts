19135e8af8b6651a7606146efabb9fd17a2bc999
protasov <protasovgleb290@gmail.com>
2024-04-10 15:45:06 +0300

    fix

--- scripts/deploy_2_RentalityGeoParser.js
+++ scripts/deploy_2_RentalityGeoParser.js
@@ -1,69 +1,57 @@
-const { ethers, network, upgrades } = require('hardhat')
+const {ethers, network, upgrades} = require('hardhat')
 const saveJsonAbi = require('./utils/abiSaver')
 const addressSaver = require('./utils/addressSaver')
-const { startDeploy, checkNotNull } = require('./utils/deployHelper')
+const {startDeploy, checkNotNull} = require('./utils/deployHelper')
 const readlineSync = require('readline-sync')
-const { getContractAddress } = require('./utils/contractAddress')
+const {getContractAddress} = require('./utils/contractAddress')
 
 async function main() {
-  const { contractName, chainId } = await startDeploy('RentalityGeoParser')
+    const {contractName, chainId} = await startDeploy('RentalityGeoParser')
 
-  if (chainId < 0) throw new Error('chainId is not set')
+    if (chainId < 0) throw new Error('chainId is not set')
 
-  let contract
+    let contract
 
-  const deployMock = async () => {
-    const mockContractName = 'RentalityGeoMock'
+    const silent = process.env.SILENT
 
-    console.log(`Deploying geo mock contact...`)
-    console.log('WARNING!! Height gas price!! Approximately 0.1 eth')
-    const contractFactory = await ethers.getContractFactory(mockContractName)
-    contract = await contractFactory.deploy()
-  }
+        if ((silent === undefined || silent === 'false') && !readlineSync.keyInYNStrict('Do you want to deploy Mock contract?')) {
+            const linkToken = '0x779877A7B0D9E8603169DdbD7836e478b4624789'
+            const oracle = '0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD'
 
-  const silent = process.env.SILENT
+            const rentalityUtilsAddress = checkNotNull(
+                getContractAddress('RentalityUtils', 'scripts/deploy_1a_RentalityUtils.js', chainId),
+                'RentalityUtils'
+            )
 
-  switch (silent === undefined || silent === 'false') {
-    case true: {
-      if (!readlineSync.keyInYNStrict('Do you want to deploy Mock contract?')) {
-        const linkToken = '0x779877A7B0D9E8603169DdbD7836e478b4624789'
-        const oracle = '0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD'
+            console.log(`Deploying RentalityGeoParser for sepolia ...`)
+            const contractFactory = await ethers.getContractFactory(contractName, {
+                libraries: {
+                    RentalityUtils: rentalityUtilsAddress,
+                },
+            })
+            contract = await contractFactory.deploy(linkToken, oracle)
+        } else {
+            const mockContractName = 'RentalityGeoMock'
 
-        const rentalityUtilsAddress = checkNotNull(
-          getContractAddress('RentalityUtils', 'scripts/deploy_1a_RentalityUtils.js', chainId),
-          'RentalityUtils'
-        )
+            console.log(`Deploying geo mock contact...`)
+            console.log('WARNING!! Height gas price!! Approximately 0.1 eth')
+            const contractFactory = await ethers.getContractFactory(mockContractName)
+            contract = await contractFactory.deploy()
+        }
 
-        console.log(`Deploying RentalityGeoParser for sepolia ...`)
 
-        const contractFactory = await ethers.getContractFactory(contractName, {
-          libraries: {
-            RentalityUtils: rentalityUtilsAddress,
-          },
-        })
-        contract = await contractFactory.deploy(linkToken, oracle)
-      } else {
-        await deployMock()
-      }
-      break
-    }
-    case false: {
-      await deployMock()
-      break
-    }
-  }
-  await contract.waitForDeployment()
-  const contractAddress = await contract.getAddress()
+    await contract.waitForDeployment()
+    const contractAddress = await contract.getAddress()
 
-  console.log(`${contractName} was deployed to: ${contractAddress}`)
-  addressSaver(contractAddress, contractName, true, chainId)
-  await saveJsonAbi(contractName, chainId, contract)
-  console.log()
+    console.log(`${contractName} was deployed to: ${contractAddress}`)
+    addressSaver(contractAddress, contractName, true, chainId)
+    await saveJsonAbi(contractName, chainId, contract)
+    console.log()
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
