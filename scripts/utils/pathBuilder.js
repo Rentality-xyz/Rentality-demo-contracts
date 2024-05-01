fb7903e1abb75a69fe525ddde89c9148b7f14d9b
protasov <protasovgleb290@gmail.com>
2024-05-01 07:04:27 +0300

    deploy 17_1

--- scripts/utils/pathBuilder.js
+++ scripts/utils/pathBuilder.js
@@ -3,12 +3,34 @@ const package = require('../../package.json')
 const pathToAddressFile = 'scripts/addressesContractsTestnets'
 const version = extractVersion()
 const format = '.json'
+
 function extractVersion() {
   return 'v' + package.version.replace(/\./g, '_')
 }
+
+function extractReadVersion() {
+  let version = process.env.READ_OLD_VERSION
+  if (version !== undefined && version.length > 0) {
+    return version
+  }
+  return 'v' + package.version.replace(/\./g, '_')
+}
+
 function buildPath() {
   return pathToAddressFile + '.' + version + format
 }
+
+function buildPathWithVersion(v = version) {
+  if (v !== version)
+    return {
+      oldPath: pathToAddressFile + '.' + v + format,
+      path: pathToAddressFile + '.' + version + format,
+    }
+  else return { path: pathToAddressFile + '.' + version + format }
+}
+
 module.exports = {
   buildPath,
+  extractReadVersion,
+  buildPathWithVersion,
 }
