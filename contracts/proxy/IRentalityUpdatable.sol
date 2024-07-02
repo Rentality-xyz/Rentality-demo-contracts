9e053665bb13ea7cd2d4aa4c2bda06b38517f46f
protasov <protasovgleb290@gmail.com>
2024-07-02 18:19:11 +0300

    fix patrol

--- contracts/proxy/IRentalityUpdatable.sol
+++
@@ -1,6 +0,0 @@
-//interface IRentalityUpdatable {
-//    function upgradeTo(address newImplementation) external;
-//
-//
-//    function upgradeToAndCall(address newImplementation, bytes memory data) external payable;
-//}
