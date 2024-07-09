97f9bd845b3a01beec55fde1d9dfc30bcd591de3
protasov <protasovgleb290@gmail.com>
2024-07-09 12:24:56 +0300

    network

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,7 +15,7 @@ module.exports = {
                    'files': one page per input Solidity file  */,
   },
 
-  defaultNetwork: 'base_sepolia',
+  defaultNetwork: 'localhost',
   networks: {
     hardhat: {
       chainId: 1337,
