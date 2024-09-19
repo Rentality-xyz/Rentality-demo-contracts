61dcdbe1d5ae97fe2e6a0dbd9d708e519a72bed1
FilippFromKR <protasovgleb290@gmail.com>
2024-09-19 17:16:16 +0300

    kycManager

---
+++ scripts/grandKYCManagerRole.js
@@ -0,0 +1,37 @@
+const saveJsonAbi = require('./utils/abiSaver')
+const { ethers } = require('hardhat')
+const addressSaver = require('./utils/addressSaver')
+const { startDeploy, checkNotNull } = require('./utils/deployHelper')
+const { Role } = require('./utils/consts')
+const { getContractAddress } = require('./utils/contractAddress')
+
+async function main() {
+  const [deployer] = await ethers.getSigners()
+  console.log(`Start script...`)
+
+  const chainId = (await deployer.provider?.getNetwork())?.chainId ?? -1
+  console.log('ChainId is:', chainId)
+
+  const rentalityUserServiceAddress = checkNotNull(
+    getContractAddress('RentalityUserService', 'scripts/deploy_1b_RentalityUserService.js', chainId),
+    'RentalityUserService'
+  )
+  const userServiceContract = await ethers.getContractAt('RentalityUserService', rentalityUserServiceAddress)
+
+  const result = await userServiceContract.manageRole(
+    Role.KYCManager,
+    '0x84E6e418B55440b8Ae17fd0326BE7f33d1553295',
+    true
+  )
+
+  console.log(result)
+
+  console.log('Success!')
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
