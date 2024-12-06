6db11543954e85205f323cad82933cf09e2dfb8e
Hlib <hlib@digisol.agency>
2024-12-06 14:20:07 +0200

    token uri

--- contracts/RentalityView.sol
+++ contracts/RentalityView.sol
@@ -65,9 +65,9 @@ contract RentalityView is UUPSUpgradeable, Initializable {
   /// @notice Retrieves the metadata URI of a car by its ID.
   /// @param carId The ID of the car.
   /// @return The metadata URI of the car.
-  // function getCarMetadataURI(uint256 carId) public view returns (string memory) {
-  //   return addresses.carService.tokenURI(carId);
-  // }
+  function getCarMetadataURI(uint256 carId) public view returns (string memory) {
+    return addresses.carService.tokenURI(carId);
+  }
 
   // not using
   /// @notice Retrieves information about all cars.
