51a8f948b7ebecc2d20e8b985d9cbb7fdfe81536
Hlib <hlib@digisol.agency>
2024-12-18 14:59:12 +0200

    fix

--- contracts/libs/RentalityQuery.sol
+++ contracts/libs/RentalityQuery.sol
@@ -368,9 +368,6 @@ library RentalityQuery {
     RentalityInsurance insuranceService = RentalityInsurance(insuranceServiceAddress);
     RentalityCarToken carService = contracts.carService;
    
-    Schemas.AvailableCarDTO memory emptyCar;
-     if(!carService.isCarAvailableForUser(carId, user, searchParams))
-    return emptyCar;
     uint fuelPrice = engineService.getFuelPriceFromEngineParams(temp.engineType, temp.engineParams);
 
     uint64 totalTripDays = uint64(Math.ceilDiv(endDateTime - startDateTime, 1 days));
