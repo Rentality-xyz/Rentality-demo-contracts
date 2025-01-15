b5fc250f1ae2588ce14e0fb9288f8c0350a46933
Hlib <hlib@digisol.agency>
2025-01-15 15:01:57 +0200

    add platform users list

--- contracts/RentalityUserService.sol
+++ contracts/RentalityUserService.sol
@@ -32,6 +32,7 @@ contract RentalityUserService is AccessControlUpgradeable, UUPSUpgradeable, IRen
   uint private kycCommission;
   mapping(address => Schemas.KycCommissionData[]) private userToKYCCommission;
   mapping(address => Schemas.AdditionalKYCInfo) private additionalKycInfo;
+  address[] private platformUsers;
 
   /// @notice Sets KYC information for the caller (host or guest).
   /// Requirements:
@@ -49,6 +50,8 @@ contract RentalityUserService is AccessControlUpgradeable, UUPSUpgradeable, IRen
 
     require(isTCPassed, 'Wrong signature.');
     Schemas.KYCInfo storage kycInfo = kycInfos[tx.origin];
+    if(kycInfo.createDate == 0 || !_alreadyInPlatformUsersList(tx.origin)) 
+      platformUsers.push(tx.origin);
 
     kycInfo.name = nickName;
     kycInfo.mobilePhoneNumber = mobilePhoneNumber;
@@ -96,6 +99,13 @@ contract RentalityUserService is AccessControlUpgradeable, UUPSUpgradeable, IRen
 
   function getMyFullKYCInfo() public view returns (Schemas.FullKYCInfoDTO memory) {
     return Schemas.FullKYCInfoDTO(kycInfos[tx.origin], additionalKycInfo[tx.origin]);
+  }
+  function getPlatformUsersKYCInfos() public view returns(Schemas.FullKYCInfoDTO[] memory result) {
+    address[] memory users = platformUsers;
+    result = new Schemas.FullKYCInfoDTO[](platformUsers.length);
+    for (uint i = 0; i < result.length; i++) {
+      result[i] = Schemas.FullKYCInfoDTO(kycInfos[users[i]], additionalKycInfo[users[i]]);
+    }
   }
   /// @notice Checks if the KYC information for a specified user is valid.
   /// @param user The address of the user to check for valid KYC.
@@ -272,6 +282,19 @@ contract RentalityUserService is AccessControlUpgradeable, UUPSUpgradeable, IRen
     else {
       _revokeRole(role, user);
     }
+  }
+
+  function getPlatformUsers() public view returns(address[] memory) {
+    return platformUsers;
+  }
+
+  function _alreadyInPlatformUsersList(address user) private view returns(bool) {
+    address[] memory users = platformUsers;
+    for (uint i = 0; i < users.length; i++) {
+      if(users[i] == user)
+      return true;
+    }
+    return false;
   }
 
   /// @notice Initializes the contract with the specified Civic verifier address and gatekeeper network ID, and sets the default admin role.
