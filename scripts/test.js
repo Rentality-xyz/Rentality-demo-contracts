98ee6d1cd4817144a2d2597bbe7be7acf5f6644d
Hlib <hlib@digisol.agency>
2025-01-16 18:47:34 +0200

    deploy

--- scripts/test.js
+++ scripts/test.js
@@ -6,7 +6,7 @@ const { startDeploy } = require('./utils/deployHelper')
 async function main() {
  const contract = await ethers.getContractAt('RentalityReferralProgram','0xBeF58aBf15D45B9c1e2c18339e2c2Dc9520a5e5D')
 
- console.log(await contract.getReadyToClaim('0x2729226a14B02D5726821d5a83d7563aCD6D3760'))
+ console.log(await contract.getMyRefferalInfo())
 }
 
 main()
