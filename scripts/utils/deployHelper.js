912a2f4ee050cd4a87fe52406ee59bead2e7ebae
Hlib <hlib@digisol.agency>
2025-01-23 14:16:46 +0200

    deploy dimo

--- scripts/utils/deployHelper.js
+++ scripts/utils/deployHelper.js
@@ -4,10 +4,7 @@ async function startDeploy(contractName) {
   console.log(`Start deploying ${contractName}`)
 
   const [deployer] = await ethers.getSigners()
-  const tx = await deployer.sendTransaction({
-    to: '0x0Ae9b1A729462CABcF73e1cD54d06dDF685F7232',
-    value: ethers.parseEther('5.0'),
-  });
+ 
   const balance = await ethers.provider.getBalance(deployer.address)
   console.log(`Deployer address is:${await deployer.getAddress()} with balance:${balance}`)
 
