a8a091db4fe8e5bf989908d1f1b26e56fcd49ead
Hlib <hlib@digisol.agency>
2025-02-19 15:27:18 +0200

    motions cloud deploy

--- contracts/abstract/IRentalityGateway.sol
+++ contracts/abstract/IRentalityGateway.sol
@@ -398,4 +398,7 @@ interface IRentalityGateway {
   function saveDimoTokenIds(uint[] memory, uint[] memory) external;
 
    function getAvaibleCurrencies() external view returns(Schemas.Currency[] memory);
+
+
+   function getMotionsCloudCaseData(uint) external view returns(Schemas.MotionsCloudCaseDataDTO memory motionsCloudCaseData);
 }
