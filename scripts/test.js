50f0a9141e4fcfdabaa444eb653ff74eccd243d7
Gleb_Protasov <94696376+HlibProtasov@users.noreply.github.com>
2025-08-14 10:26:11 +0300

    add hooks

--- scripts/test.js
+++ scripts/test.js
@@ -5,6 +5,8 @@ const addressSaver = require('./utils/addressSaver')
 const { checkNotNull, startDeploy } = require('./utils/deployHelper')
 const { keccak256 } = require('hardhat/internal/util/keccak')
 const { zeroHash, ethToken, emptyLocationInfo } = require('../test/utils')
+const { Actions, V4Planner } = require('@uniswap/v4-sdk')
+  const { CommandType, RoutePlanner } = require('@uniswap/universal-router-sdk')
 
 const tripsFilter = {
   paymentStatus: 0,
@@ -17,33 +19,62 @@ const tripsFilter = {
 async function main() {
 
   const [deployer] = await ethers.getSigners()
-  const contractFactory = await ethers.getContractFactory('RentalitySwaps')
+  const contractFactory = await ethers.getContractFactory('AccessSenderHook')
   let linkToken = "0xE4aB69C077896252FAFBD49EFD26B5D171A32410"
   let amountIn = 25000;
+  let poolManager = "	0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408"
+  let weth = "0x4200000000000000000000000000000000000006"
 
-  console.log("DD: ",deployer.address)
+  let contract =  await contractFactory.deploy(poolManager, weth)
 
-  // let uniswapFactory = await ethers.getContractAt('UniswapFactory','0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24')
-  // let poolCreation = await uniswapFactory.createPool(linkToken,'0x4200000000000000000000000000000000000006', 100);
+  let abiCoder = ethers.utils.defaultAbiCoder
+let rentalityGateway = await ethers.getContractAt('IRentalityGateway','0xB257FE9D206b60882691a24d5dfF8Aa24929cB73')
 
-  // console.log(poolCreation)
-  // const contract = await upgrades.deployProxy(contractFactory, ['0x94cC0AaC535CCDB3C01d6787D6413C739ae12bc4', '0x4200000000000000000000000000000000000006',linkToken])
-  // await contract.waitForDeployment()
+let data = rentalityGateway.interface.encodeFunctionData('payKycCommission', [zeroHash])
 
-  let contract = await ethers.getContractAt('RentalitySwaps', '0x5033F0ECf22bC70710E3a9309BC64B26574913FE')
-  let iercContract = await ethers.getContractAt("contracts/payments/abstract/IERC20.sol:IERC20", linkToken)
- let approvetx = await iercContract.approve(await contract.getAddress(), amountIn)
+let updatedData = abiCoder.encode(['bytes', 'address', 'uint256'], [data, deployer.address, amountIn])
+   let config = {
+  poolKey: {
+      currency0: linkToken,
+      currency1: ethToken,
+      fee: 500,
+      tickSpacing: 10,
+      hooks: await contract.getAddress(),
+  },
+  zeroForOne: true,
+  amountIn: amountIn, 
+  amountOutMinimum: "minAmountOut",
+  hookData: updatedData
+}
+  const v4Planner = new V4Planner()
+  const routePlanner = new RoutePlanner()
+  
+  // Set deadline (1 hour from now)
+  const deadline = Math.floor(Date.now() / 1000) + 3600
+  
+  v4Planner.addAction(Actions.SWAP_EXACT_IN_SINGLE, [config]);
+  v4Planner.addAction(Actions.SETTLE_ALL, [config.poolKey.currency0, CurrentConfig.amountIn]);
+  v4Planner.addAction(Actions.TAKE_ALL, [config.poolKey.currency1, CurrentConfig.amountOutMinimum]);
+  const encodedActions = v4Planner.finalize()
+  
+  routePlanner.addCommand(CommandType.V4_SWAP, [v4Planner.actions, v4Planner.params])
+  
+
+  
+  const tx = await universalRouter.execute(
+      routePlanner.commands,
+      [encodedActions],
+      deadline,
+      {}
+  )
+  
+  const receipt = await tx.wait()
+  console.log('Swap completed! Transaction hash:', receipt.transactionHash)
 
   console.log(await approvetx.wait())
 
 
-  let args = contract.interface.encodeFunctionData('swapExactInputSingle',[linkToken, amountIn, deployer.address])
-  console.log('data:', args)
-  let result = await contract.swapExactInputSingle(linkToken, amountIn, deployer.address)
-// 
-  console.log(result)
-  const balance = await ethers.provider.getBalance(await contract.getAddress());
-  console.log("BALANCE: ", balance)
+
 
 }
 
