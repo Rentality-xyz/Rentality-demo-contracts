22f88f9b524f2696f0ba3d1d11058cc861ddd79e
Gleb_Protasov <hlibaltrecipe@gmail.com>
2025-11-18 15:16:47 +0200

    usdc deploy

--- scripts/testl0.js
+++ scripts/testl0.js
@@ -1,19 +1,8 @@
 const { ethers, upgrades } = require('hardhat')
 
 async function main() {
-// const gatewat = await ethers.getContractAt("RentalitySender",'0x1C97042e54bAa7ba8d58520b855ec67C6Ff4286C')
-
-// console.log(await gatewat.setGasLimit(10_000_000))
-
-console.log((Math.sqrt(4000) * 2 ** 96).toString());
-
-    
-}
-
-async function checkError(error) {
-const contract = await ethers.getContractAt("ARentalitySender", '0xF103293cffA7a6998Be917DCC1A0174540B418Fc')
-
-console.log(contract.interface.parseError(error.data))
+const rentalitySwaps = await ethers.getContractAt('RentalityTestUSDC', '0x23805809f496FfB869913FF38667F20C39088225')
+console.log(await rentalitySwaps.transfer('0xd89c758da61e45eee4770888ebe04372f0d55a6a', BigInt(10000 * 10 ** 6)))
 }
 
 main()
